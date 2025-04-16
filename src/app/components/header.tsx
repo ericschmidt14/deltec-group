85ac20cf6637a1fdca3e3791f23ba65bb7992f91
E R I C S C H M I D T <info@ericschmidt.de>
2025-04-16 13:13:24 +0200

    make header logo stand out

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,12 +5,15 @@ import { scrollTo } from "../lib/utils";
 export default function Header() {
   return (
     <header className="fixed top-0 z-50 w-full flex justify-center px-4">
-      <Link href="/">
+      <Link
+        href="/"
+        className="bg-white/50 px-4 pt-2 shadow-xl backdrop-blur-md"
+      >
         <Image
           src="/logo.svg"
           alt="Logo"
           width={120}
-          height={64}
+          height={30}
           onClick={() => scrollTo("top")}
           className="cursor-pointer"
         />
